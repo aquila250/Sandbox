a0e388bf6fa63c8eb3b0eee463c0e28b3372cc2d
Tim Shephard <aq45@pm.me>
2019-02-25 19:55:39 -0800

    messing around with lua

--- Examples-lua/helloWorld.lua
+++ Examples-lua/helloWorld.lua
@@ -16,7 +16,8 @@ b = 30
 
 io.write (
   "Hello world, from ",_VERSION,"!\n\n",
-  "This program is an example, & just for fun\n\n",
+  "It's ", os.date(),
+  ", and this program is an example of some lua coding. Its just for fun\n\n",
   "The value of \"a\" is: ", a, "\n",
   "The value of \"b\" is: ", b, "\n\n"
 )
@@ -28,13 +29,34 @@ b, a = a, b
 io.write (
   "Now, the value of \"a\" is: ", a, "\n",
   "And, the value of \"b\" is: ", b, "\n\n",
-  "Ok, Lets try loops:\n\n"
+  "Ok, Lets try some math stuff:\n\n"
+)
+io.write (
+  "Here is a random number between 1 and 10: ",
+  math.random(1,10), "\n\n"
 )
 
+io.write (
+  "OK. So, here's a guessing game. pick a number between 1 and 10.\n\n",
+  "Guess: "
+)
 repeat
-exit = false
-exit = io.read ())
-until ( exit == true )
+  guess = io.read()
+
+until tonumber(guess) == math.random(1,10)
+print("You got it") 
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 io.write ("\n\n - EOF - \n\n")
