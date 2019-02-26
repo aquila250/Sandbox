bc335d5b7be5c4d764fbef115509c22a26d14877
Tim Shephard <aq45@pm.me>
2019-02-25 17:18:33 -0800

    added lua example, and organized directories

--- pyExamples/helloWorld.py
+++
@@ -1,7 +0,0 @@
-# Modules
-# Variables
-# There are 5 data types in .py:
-# numbers, strings, lists, tuples, dictionaries/maps
-x = "Hello World =^)"
-# 
-print(x)
--- pyExamples/strings.py
+++
@@ -1,9 +0,0 @@
-# modules
-
-# variables
-quote = "\""
-smiley = "=^)"
-# Strings
-print("\"hello\"")
-print("\n")
-print("%s %s" % ("Hi there,", smiley))
